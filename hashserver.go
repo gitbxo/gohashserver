00985ab2a2c8255b25c129c56bb82a6f60801323
Sanjay Khanna <sanjay.bxo@gmail.com>
2021-01-21 00:17:21 -0800

    return request stats when requested

--- hashserver.go
+++ hashserver.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "net/http"
     "sync/atomic"
+    "time"
 )
 
 // atomic wrapper around int64
@@ -20,6 +21,8 @@ func (v *Int64) Add(i int64) int64 {
 }
 
 var hashCounter Int64 = *NewInt64(0)
+var hashTime Int64 = *NewInt64(0)
+var hashMap = make(map[int64]string)
 
 
 func hashGET(w http.ResponseWriter, req *http.Request) {
@@ -27,19 +30,51 @@ func hashGET(w http.ResponseWriter, req *http.Request) {
 }
 
 
-func hashPOST(w http.ResponseWriter, req *http.Request) {
-    var newId = hashCounter.Add(1)
-    // defer saveHashValue(hashValue)
-    fmt.Fprintf(w, "hashPOST %d\n", newId)
+func hashPOST(w http.ResponseWriter, start time.Time, password string) {
+    newId := hashCounter.Add(1)
+    // Delay saving the hash value
+    defer func() { go saveHashValue(newId, password) }()
+    fmt.Fprintf(w, "%d\n", newId)
+
+    elapsed := time.Since(start)
+    // Add the elapsed time in microseconds
+    hashTime.Add(elapsed.Microseconds())
 }
 
 
+func saveHashValue(key int64, value string) {
+    // sleep 5 seconds
+    time.Sleep(5 * time.Second)
+    hashMap[key] = value
+}
+
+
+// For example: curl http://localhost:8080/stats should return:
+// {"total": 1, "average": 123}
+func statsHandler(w http.ResponseWriter, req *http.Request) {
+    requests := hashCounter.c
+    // calculate average time in microseconds
+    if requests > 0 {
+      avgTime := hashTime.c / requests
+      fmt.Fprintf(w, "{\"total\": %d, \"average\": %d}\n", requests, avgTime)
+    } else {
+      fmt.Fprintf(w, "{\"total\": 0, \"average\": 0}\n")
+    }
+}
+
+
+
 func hashHandler(w http.ResponseWriter, req *http.Request) {
+    start := time.Now()
+
     if req.Method == "GET" {
       hashGET(w, req)
     } else {
       if req.Method == "POST" {
-        hashPOST(w, req)
+        password := req.PostFormValue("password")
+        if len(password) > 0 {
+          hashPOST(w, start, password)
+        }
       } else {
         http.NotFound(w, req)
       }
@@ -49,5 +84,6 @@ func hashHandler(w http.ResponseWriter, req *http.Request) {
 
 func main() {
     http.HandleFunc("/hash", hashHandler)
-    http.ListenAndServe(":8090", nil)
+    http.HandleFunc("/stats", statsHandler)
+    http.ListenAndServe(":8080", nil)
 }
